383e10b258e75042a8c7953e3b4cd80d507fb1a4
am910021 <am910021@gmail.com>
2021-02-08 21:04:08 +0800

    modify config

--- module/callback/checkpointone.py
+++ module/callback/checkpointone.py
@@ -5,8 +5,9 @@ from keras.callbacks import Callback
 
 class TrainCallback(TrainCallbackAbstract):
     def create_callback(self) -> ModelCheckpoint:
+        ep_srt = '{epoch:0' + len(self.config.MAX_EPOCHS) + 'd}.'
         return ModelCheckpoint(
-            self.config.CHECKPOINT_PATH + "/" + self.config.CNN_MODEL_FILE + self.get_time_path() + '{epoch:05d}.' + self.config.CHECKPOINT_MODEL,
+            self.config.CHECKPOINT_PATH + "/" + self.config.CNN_MODEL_FILE + self.get_time_path() + ep_srt + self.config.CHECKPOINT_MODEL,
             monitor='val_loss',
             verbose=1,
             save_best_only=True,
--- module/callback/checkpointtwo.py
+++ module/callback/checkpointtwo.py
@@ -4,8 +4,9 @@ from tensorflow.keras.callbacks import ModelCheckpoint
 
 class TrainCallback(TrainCallbackAbstract):
     def create_callback(self) -> ModelCheckpoint:
+        ep_srt = '{epoch:0' + len(self.config.MAX_EPOCHS) + 'd}.'
         return ModelCheckpoint(
-            self.config.CHECKPOINT_PATH + "/" + self.config.CNN_MODEL_FILE + self.get_time_path() + '{epoch:05d}.' + self.config.CHECKPOINT_WEIGHTS,
+            self.config.CHECKPOINT_PATH + "/" + self.config.CNN_MODEL_FILE + self.get_time_path() + ep_srt + self.config.CHECKPOINT_WEIGHTS,
             monitor='val_loss',
             verbose=1,
             save_best_only=True,
